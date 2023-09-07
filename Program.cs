aa7e6645cabfea3b235d4d40c1bc3c61b9133a5e
MuhammadumarMM <botirovmuhammadumar33.gmail.com>
2023-09-07 20:52:11 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿//// begin 1
+//Console.Write("Kvadrat tomonini kiriting: ");
+//int a = int.Parse(Console.ReadLine());
+//int P;
+//P = 4 * a;
+//Console.WriteLine("Peremetri = " + P);
+
+
+//// begin 2
+//Console.Write("Kvadrat tomonini kiriting: ");
+//int b = int.Parse(Console.ReadLine());
+//int S;
+//S = b * b;
+//Console.WriteLine("Yuzasi = " +  S);
+
+
+//// begin 3
+//Console.Write("a Tomonini kiriting: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.Write("b Tomonini kiriting: ");
+//int b = int.Parse(Console.ReadLine());
+//int S;
+//int P;
+//S = a * b;
+//P = 2 * ( a + b );
+//Console.WriteLine("Yuzasi : " + S);
+//Console.WriteLine("Peremetri : " +  P);
+
+
+//// begin 4 
+//const double PI = 3.14;```
+//Console.Write("Raidusni kiriting: ");
+//int R = int.Parse(Console.ReadLine());
+//double L;
+//L = PI * R;
+//Console.WriteLine("Aylana uzunligi: " + L);
+
+
+////begin 5
+//Console.Write("Kub qirrasini kiriting: ");
+//int a = int.Parse(Console.ReadLine());
+//int V;
+//int S;
+//V = a * a * a;
+//S = 6 * a * a;
+//Console.WriteLine("Kub hajmi: " + V);
+//Console.WriteLine("Kub to'la sirti: " + S);
+
+
+////begin 6
+//Console.Write("Paralelopipedning qirralarini kiriting: ");
+//var num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//int V;
+//int S;
+//V = num[0] * num[1] * num[2];
+//S = 2 * (num[0] * num[1] + num[1] * num[2] + num[0] * num[2]);
+//Console.WriteLine(" Hajmi: " + V);
+//Console.WriteLine(" Yuzasi: " + S);
+
+
+////begin 7
+//const double PI = 3.14;
+//Console.Write("Raidusni kiriting: ");
+//int R = int.Parse(Console.ReadLine());
+//double L;
+//double S;
+//L = PI * R;
+//S = PI * R * R; 
+//Console.WriteLine("Doira uzunligi: " + L  + "  Doira yuzasi: " + S);
+
+////begin 8
+//float a = float.Parse(Console.ReadLine());
+//float b = float.Parse(Console.ReadLine());
+//float c;
+//c = (a + b) / 2;
+//Console.WriteLine(c);
+
+
+////begin 9
+//float a = float.Parse(Console.ReadLine());
+//float b = float.Parse(Console.ReadLine());
+//double c;
+//if (a > 0 && b > 0)
+//{
+//    c = Math.Sqrt(a * b);
+//    Console.WriteLine(c);
+//}
+//else
+//{
+//    Console.WriteLine("Narmalniy son kiriting brat!!! f.e  3 bilan 5");
+//}
+
+
+////begin 10
+//float a = float.Parse(Console.ReadLine());
+//float b = float.Parse(Console.ReadLine());
+//float c;
+//float d;
+//float e;
+//if (a != 0 && b != 0)
+//{
+//    c = a + b;
+//    d = a - b;
+//    e = a * b;
+//    Console.WriteLine("Yigindisi: " + c * c  + "   Ayrimasi: " + d * d + "   Kopaytmasi: " + e * e);
+//}
+
